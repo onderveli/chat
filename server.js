38822316654b6aee2170c2d8b6915926c8c73468
onderveli <onderveli123@gmail.com>
2016-03-08 21:19:03 +0200

    d

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/index.html'));
-
-// views is directory for all template files
-app.set('b', __dirname + '/b.html');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('index.html');
-});
-
-app.listen(app.get('port'), function() {
-      socket.on('solOk', function (data) {
-		console.log('SolOk');
-        io.sockets.emit('SolOk', data);
-    });
-	socket.on('sagOk', function (data) {
-		console.log('SagOk');
-        io.sockets.emit('SagOk', data);
-    });
-});
+// This is the main file of our chat app. It initializes a new 
+// express.js instance, requires the config and routes files
+// and listens on a port. Start the application by running
+// 'node app.js' in your terminal
 
 
+var express = require('express'),
+	app = express();
 
+// This is needed if the app is run on heroku:
 
+var port = process.env.PORT || 8080;
+
+// Initialize a new socket.io object. It is bound to 
+// the express app, which allows them to coexist.
+
+var io = require('socket.io').listen(app.listen(port));
+
+// Require the configuration and the routes files, and pass
+// the app and io as arguments to the returned functions.
+
+require('./config')(app, io);
+require('./routes')(app, io);
+
+console.log('Your application is running on http://localhost:' + port);
