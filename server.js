3ed0f5e5d681d4455fa47fceb3d4a9f7c0cb854b
onderveli <onderveli123@gmail.com>
2016-03-12 17:33:58 +0200

    sd

--- server.js
+++ server.js
@@ -11,7 +11,23 @@ var io = require('socket.io').listen(app.listen(port)); // this tells socket.io 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');//Ana Dizine yönlendir
 });
+app.route('/upload')
+    .post(function (req, res, next) {
 
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            fstream = fs.createWriteStream(__dirname + '/img/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {    
+                console.log("Upload Finished of " + filename);              
+                res.redirect('back');           //where to go next
+            });
+        });
+    });
 io.sockets.on('connection', function(socket){//Socket ile bağlantı kuruldu.
 	socket.on('new user', function(data, callback){
 		if (nicknames.indexOf(data) != -1){
