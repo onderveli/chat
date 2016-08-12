245f6bad55c45665a9053e4c7d1e8d976a509253
YazilimciAkli <onderveli123@gmail.com>
2016-08-12 11:26:45 +0300

    Send
    Send

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var	writes=[];//yazıyor listesi
 var port = process.env.PORT || 8080;
 
 var io = require('socket.io').listen(app.listen(port)); // this tells socket.io to use our express server
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'https://n1plcpnl0015.prod.ams1.secureserver.net:3306',
+  user     : 'chatv23',
+  password : '123456',
+  database : 'ChatAppV1'
+});
+
 
 
 
@@ -57,6 +65,16 @@ io.sockets.on('connection', function(socket){//Socket ile bağlantı kuruldu.
 	}
 	socket.on('send message', function(data){//Socket açtık ve içine data değerini aldık
 		io.sockets.emit('new message', {msg: data, nick: socket.nickname});//Data'yı socket üzerinden istemcilerdeki fonksiyona yolladık
+		connection.connect();
+
+		connection.query('INSERT INTO logs (name,msg) VALUES ("'+name+'","'+msg+'")', function(err, rows, fields) {
+		  if (!err)
+			console.log('The solution is: ', rows);
+		  else
+			console.log('Error while performing Query.');
+		});
+
+		connection.end();
 		deleteNick(socket.nickname);
 	});
 	socket.on('write', function(data){//Socket açtık ve içine data değerini aldık
