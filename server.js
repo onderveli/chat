9b7f0c4e7ace9fca646ea93b8cb9bd257c9af4e9
YazilimciAkli <onderveli123@gmail.com>
2016-08-12 15:52:19 +0300

    a
    a

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 var io = require('socket.io').listen(app.listen(port)); // this tells socket.io to use our express server
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'https://n1plcpnl0015.prod.ams1.secureserver.net:3306',
+  host     : '160.153.16.13:3306',
   user     : 'chatv23',
   password : '123456',
   database : 'ChatAppV1'
